8587ccf8581d487b81209d8697fa0a2ae231f735
Hayden Ratliff <haydenratliff@virginia.edu>
2021-09-08 15:59:18 -0400

    Hayden final edits

--- Graph 2.R
+++ Graph 2.R
@@ -1,19 +1,26 @@
 # Hayden's File
 
 library(tidyverse)
+# load in packages
 
 ## STEP 1 : Madeleine
 cereal <- read_csv("cereal.csv")
+# read in csv
 
 ## STEP 2: Audrey
 
 # loading ggplot
 library(ggplot2)
+# load in ggplot2
 
 # creating a scatterplot to compare sugars and rating
 plot1 <- ggplot(cereal, aes(x=sugars, y=rating)) + geom_point()
-plot1
-
-
+# create plot
 
 # STEP 3: Hayden
+finished_plot = plot1 + ggtitle("Cereal rating vs sugars")
+# add title to plot
+finished_plot
+# display finished plot
+
+# note: Hayden addedd comments to each line of code
